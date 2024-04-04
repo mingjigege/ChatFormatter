d6ab0d80dcf35dd23c5ca60fc7a0c59411d6096c
铭记mingji <109053797+mingjigege@users.noreply.github.com>
2024-04-04 18:19:59 +0800

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -2,10 +2,15 @@
 #include "Global.h"
 
 std::string defaultConfig = R"({
-    "ChatFormat": "[%chatformatter_dimension%] {player} >> {message}",
+    "ChatFormat": "%onetitle%§r|%player_org%§r|§a%player_ping%ms§r|§b%player_device%§r|{player} §l§e>>§r {message}",
     "DimensionNameMap": {
-        "Overworld": "Overworld",
-        "Nether": "Nether",
-        "TheEnd": "The End"
+        "Overworld": "主世界",
+        "Nether": "下界",
+        "TheEnd": "末地"
+    },
+    "ban":{
+        "妈", 
+        "操",  
+        "卡"
     }
 })";
